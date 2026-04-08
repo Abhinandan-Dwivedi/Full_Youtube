6b435a3744e45b5ebe648b6616c7894ce37df421
Abhinandan <dwabhi2006@gmail.com>
2026-04-08 17:46:16 +0530

    port define

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,11 +6,13 @@ import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
+const port = process.env.PORT || 5000;
+
 connectDB()
 .then(() => {
     console.log("Database connected successfully");
-   app.listen(process.env.PORT, () => {
-       console.log(`Server is running on port ${process.env.PORT}`);
+   app.listen(port , () => {
+       console.log(`Server is running on port ${port}`);
    });
 })
 .catch((error) => {
